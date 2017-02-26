30a061e9a95431f2dd09341ec66a178917c78d0a
egkozlov <e.kozlov@softteco.com>
2017-02-26 16:09:49 +0300

    Initial commit

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AngularRouting2Page } from './app.po';
+
+describe('angular-routing2 App', function() {
+  let page: AngularRouting2Page;
+
+  beforeEach(() => {
+    page = new AngularRouting2Page();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
