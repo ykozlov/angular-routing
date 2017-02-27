62c9b5b6ff8a329139fabccdabd6fa1785b6f57b
egkozlov <e.kozlov@softteco.com>
2017-02-27 21:39:58 +0300

    Add hanling optional url parameters

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute, Params } from '@angular/router';
 @Component({
   templateUrl: './user-list.component.html',
+
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  optionalParameter: any;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
-    console.log('users init');
+    this.route.params
+      .subscribe((params: Params) => this.optionalParameter = params['foo'] || null);
   }
 
 }
